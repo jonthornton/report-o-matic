a4c3e0600fc15edcaed8456f5f9b926d9c13855e
Jon Thornton <thornton.jon@gmail.com>
2014-08-12 18:43:40 -0400

    Added a couple more fields

--- app.py
+++ app.py
@@ -13,13 +13,36 @@ def describe_score(score, low=0, high=100):
 
 app.jinja_env.globals.update(describe_score=describe_score)
 
-class DummyForm(Form):
-    name = wtforms.TextField('Your name', validators = [validators.InputRequired()])
-    score = wtforms.IntegerField('Score (0-100)', validators = [validators.InputRequired()])
+class RequiredIf(validators.Required):
+    # a validator which makes a field required if
+    # another field is set and has a truthy value
+
+    def __init__(self, other_field_name, *args, **kwargs):
+        self.other_field_name = other_field_name
+        super(RequiredIf, self).__init__(*args, **kwargs)
+
+    def __call__(self, form, field):
+        other_field = form._fields.get(self.other_field_name)
+        if other_field is None:
+            raise Exception('no field named "%s" in form' % self.other_field_name)
+        if bool(other_field.data):
+            super(RequiredIf, self).__call__(form, field)
+
+class InputForm(Form):
+    name = wtforms.TextField('Name', validators = [validators.InputRequired()])
+    birthday = wtforms.DateField('Birthday', format='%m/%d/%Y', validators = [validators.InputRequired()])
+    school = wtforms.TextField('School', validators = [validators.InputRequired()])
+
+    grade_opts = [(str(i), str(i)) for i in range(1,13)]
+    grade_opts.insert(0, ('K', 'K'))
+    grade = wtforms.SelectField('Grade', coerce=str, choices=grade_opts, validators = [validators.InputRequired()])
+
+    examiner = wtforms.TextField('Examiner', validators = [validators.InputRequired()])
+
 
 @app.route('/', methods=['GET', 'POST'])
 def report_form():
-    form = DummyForm()
+    form = InputForm()
 
     if form.validate_on_submit():
         return render_template('report.html', form=form)
